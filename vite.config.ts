07935fa71ad64004143cb36564d0050fad86bbfe
Oleksandr Cheban <cebanoleksandr@gmail.com>
2026-02-24 13:01:54 +0200

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/zenbit-react-client/',
   plugins: [
     tailwindcss(),
     react()
